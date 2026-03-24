12b39f4d30cce62498aa5baf6018d5c60ac72544
23f2004010 <23f2004010@ds.study.iitm.ac.in>
2026-03-24 23:36:14 +0530

    final

---
+++ convert_pdf.py
@@ -0,0 +1,11 @@
+import fitz
+
+doc = fitz.open("12306205summertraining.pdf")
+page = doc.load_page(0)
+pix = page.get_pixmap(dpi=150)
+pix.save("summertraining.png")
+
+doc2 = fitz.open("CODEIITM.pdf")
+page2 = doc2.load_page(0)
+pix2 = page2.get_pixmap(dpi=150)
+pix2.save("CODEIITM.png")
